0c9b91eea95055e5f367497430beb2e98412f230
Depthecho <51miha7@gmail.com>
2025-06-02 21:56:57 +0300

    added text-detail but its not finished yet

--- textspage/urls.py
+++ textspage/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.texts_page, name='texts_page'),
-    #path('<int:pk>/', views.text_detail, name='text_detail'),
+    path('<int:pk>/', views.text_detail, name='text_detail'),
     ]
--- textspage/views.py
+++ textspage/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import Text
 from .services import TextService
+from django.shortcuts import render, get_object_or_404
 
 
 def texts_page(request):
@@ -7,3 +9,10 @@ def texts_page(request):
     params = TextService._extract_params(request)
     context = TextService.get_context(page_obj, params)
     return render(request, 'textspage/texts_page.html', context)
+
+def text_detail(request, pk):
+    text = get_object_or_404(Text, pk=pk)
+    context = {
+        'text': text
+    }
+    return render(request, 'textspage/text_detail.html', context)
